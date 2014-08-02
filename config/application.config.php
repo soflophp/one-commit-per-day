5fb4b7cbde2d1332fa10c91586b593a209b2c65f
aculp <adamculp@uws.net>
2014-08-01 22:17:51 -0400

    Added the commit module

--- config/application.config.php
+++ config/application.config.php
@@ -4,6 +4,7 @@ return array(
     'modules' => array(
         'Application',
         'Promise',
+        'Commit',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
