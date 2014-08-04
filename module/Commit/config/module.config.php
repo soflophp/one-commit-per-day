ff1d1c06cf7257aaef252ffaa9097fd7f92e3f50
aculp <adamculp@uws.net>
2014-08-04 12:53:15 -0400

    Updated template directory for view manager

--- module/Commit/config/module.config.php
+++ module/Commit/config/module.config.php
@@ -44,7 +44,7 @@ return array(
     ),
     'view_manager' => array(
         'template_path_stack' => array(
-            'ZendSkeletonModule' => __DIR__ . '/../view',
+            'Commit' => __DIR__ . '/../view',
         ),
     ),
 );
